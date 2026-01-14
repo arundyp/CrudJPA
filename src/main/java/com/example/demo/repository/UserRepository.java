69bdd986c7ad8b98f572fd86beb0a0c10705bd39
Arun kushwaha <65993965+arundyp@users.noreply.github.com>
2026-01-14 23:09:25 +0530

    Update UserRepository.java

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -10,7 +10,7 @@ public interface UserRepository extends JpaRepository<User,Integer> {
     //kushwaha
     public List<User> findByCity(String city);
     public  Boolean existsByEmail(String email);
-    public List<User> findByRole(String role);
+    
     //arun
     public List<User> findByCityAndRole(String city, String role);
     public List<User> findByDateBetweenAndRole(LocalDate startDate, LocalDate endDate, String role);
